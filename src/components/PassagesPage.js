7d000348b2f2808cdeffc00197ef0742084a0de2
lwooden <lowell.wooden@gmail.com>
2020-05-11 16:48:13 -0400

    display header based on the category you selected

--- src/components/PassagesPage.js
+++ src/components/PassagesPage.js
@@ -9,12 +9,14 @@ class CategoryDetailsPage extends Component {
 
         this.state = {
             details: [],
+            category: ''
         }
         // console.log(props) // check props that are available to me
     }
 
 componentDidMount() {
     this.getPassagesByCategory()
+    this.setCategory()
 }
 
 // I have access to the "id" of the category that was clicked on the last page through the prop
@@ -25,15 +27,29 @@ getPassagesByCategory() {
     axios.get(`http://localhost:3001/api/categories/${categoryId}/passages`)
     .then(response => {
         this.setState({details: response.data}, () => {
-            console.log(this.state)
+            // console.log(this.state)
+        })
+    })
+    .catch(err => console.log)
+}
+
+setCategory() {
+    let categoryId = this.props.match.params.id
+    axios.get(`http://localhost:3001/api/categories/${categoryId}`)
+    .then(response => {
+        this.setState({category: response.data}, () => {
+            // console.log(this.state)
         })
     })
     .catch(err => console.log)
 }
 
 
+
+
     render(){
         // console.log(this.state.details)
+        // console.log(this.state.category)
         const passageList = this.state.details.map((passage, i) => {
             return(
                 <div>
@@ -44,6 +60,7 @@ getPassagesByCategory() {
         })
         return (
             <div>
+            <h1>Verses About "{this.state.category.categoryName}"</h1>
             <h3>{passageList}</h3>
             </div>
         )
